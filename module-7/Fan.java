1c61ef2975ee789c1888e3f34f2462373fadeb21
gisellaadair <eladair@my365.bellevue.edu>
2026-03-15 20:56:54 -0400

    Redo assignment Mod 7

---
+++ module-7/Fan.java
@@ -0,0 +1,61 @@
+public class Fan {
+
+    public static final int SLOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int FAST = 3;
+
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    // Default constructor
+    public Fan() {
+        speed = SLOW;
+        on = false;
+        radius = 6;
+        color = "white";
+    }
+
+    // Constructor with parameters
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    // Getters
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    // Setters
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
